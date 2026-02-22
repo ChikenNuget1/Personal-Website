f40f54b78b91c4076f7fd3fc48bdfb272c861e26
ChikenNuget1 <samuel.choi111@gmail.com>
2026-02-22 13:59:43 +1300

    Finished header for now. Very simple but it's enough for now

--- personal-website/components/header.tsx
+++ personal-website/components/header.tsx
@@ -1,11 +1,16 @@
 
 export default function Header(){
     return (
-        <header>
-            <a href="/">
-                Samuel Choi
-            </a>
-            <nav>
+        <header className="flex justify-between p-10 space-x-4 mx-25 items-center text-4xl font-heading">
+            <div className="flex justify-center items-center">
+                <a href="/" className="w-25">
+                    <img src="/Moon.svg" alt="Logo"/>
+                </a>
+                <a href="/" className="">
+                    Samuel Choi
+                </a>
+            </div>
+            <nav className="flex justify-end space-x-25 mr-5">
                 <a href="#about">
                     About Me
                 </a>
